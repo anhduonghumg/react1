492703bbb48f551fb4c81c7f81eb1d3a5ce0bb45
showme <showmeigmail.com>
2020-11-02 18:01:35 +0700

    upload file

---
+++ ex1.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bài tập PHP</title>
+</head>
+
+<body>
+    <style>
+        #wp-content {
+            width: 480px;
+            margin: 0px auto;
+            text-align: center;
+            background: black;
+        }
+
+        img {
+            width: 100%;
+        }
+        .box {
+            color: white;
+
+        }
+
+        p {
+            text-align: left;
+        }
+
+        span {
+            text-align: right;
+        }
+    </style>
+    <div id="wp-content">
+        <div id="content">
+            <div class="box">
+                <img src="anh.jpg" height="300" width="500" alt="">
+                <p><strong>Việt Nam</strong> đất nước ta ơi</p>
+                <p>Mênh mông biển lúa đất trời đẹp hơn</p>
+                <span>_Tố Hữu_</span>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ ex2.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bài tập 2</title>
+</head>
+
+<body>
+    <div id="content">
+        <style>
+            table {
+                width: 100%;
+                border: 1px solid black;
+            }
+
+            table th {
+                border-right: 1px solid black;
+            }
+        </style>
+        <table>
+            <tbody>
+                <tr>
+                    <th>
+                        <p>Bà còng đi chợ trời mưa</p>
+                        <p>Cái tôm cái tép đi đưa bà còng</p>
+                    </th>
+                    <th>
+                        <p>Trăm năm trong cõi người ta</p>
+                        <p>Chữ tài chữ mệnh khó mà vậy sao</p>
+                    </th>
+                </tr>
+            </tbody>
+        </table>
+        <div style="width:33% ; float:left"><span>Bà còng</span> đi chọ...</div>
+        <div style="width:33% ; float:left">Trăm năm trong cõi người ta</div>
+        <div style="width:33% ; float:left">Dãy là cột thứ 3</div>
+    </div>
+    <div id="footer" style="width:100% ; float:left ; margin-top:50px">
+        <p style="text-align:center">VÍ DỤ HTML</p>
+    </div>
+</body>
+
+</html>
